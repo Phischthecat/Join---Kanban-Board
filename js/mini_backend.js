53bfbbc852efe445aa2a505efd15d6c3077aed93
Phil Schmucker <p.schmuck.er@web.de>
2023-01-03 19:58:37 +0100

    changes

--- js/mini_backend.js
+++ js/mini_backend.js
@@ -1,6 +1,6 @@
 let jsonFromServer = {};
 let BASE_SERVER_URL;
-setURL('https://rico-denkewitz.developerakademie.net/smallest_backend_ever');
+setURL('https://philschmucker.de/smallest_backend_ever');
 
 const backend = {
   setItem: function (key, item) {
